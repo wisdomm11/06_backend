e31d81b3b317f4683464653bd286d993c630fdf4
wisdomm11 <gurojihoon@gmail.com>
2025-06-19 16:10:06 +0900

    2025_0619_1610_2222222

--- shop_thymleaf/src/main/java/com/shop/controller/AnswerController.java
+++ shop_thymleaf/src/main/java/com/shop/controller/AnswerController.java
@@ -1,15 +1,18 @@
 package com.shop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shop.dto.AnswerForm;
+import com.shop.entity.Answer;
 import com.shop.entity.Question;
 import com.shop.service.AnswerService;
 import com.shop.service.QuestionService;
@@ -27,6 +30,21 @@ public class AnswerController {
 	// 빈 주입
 	private final AnswerService answerService;
 	private final QuestionService questionService;
+	
+	@GetMapping("/create/{id}")
+	public String answerList(
+			Model model,
+			@RequestParam(value="page", defaultValue="0") Integer page
+			) {
+		
+		Page<Answer> paging = this.answerService.getList(page);
+		
+		model.addAttribute("paging", paging);
+		
+		return "question_detail";
+
+	}
+	
 	
 	// 답변 등록
 	@PostMapping("/create/{id}")
--- shop_thymleaf/src/main/java/com/shop/service/AnswerService.java
+++ shop_thymleaf/src/main/java/com/shop/service/AnswerService.java
@@ -1,7 +1,13 @@
 package com.shop.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.shop.entity.Answer;
@@ -29,5 +35,15 @@ public class AnswerService {
 		answerRepository.save(answer);
 		
 	}
+	
+    // 스프링 프레임워크 도메인 // 페이지 !!
+    // 질문 리스트 // 페이징 처리
+    public Page<Answer> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+    	
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.answerRepository.findAll(pageable);
+    }
 	
 }
